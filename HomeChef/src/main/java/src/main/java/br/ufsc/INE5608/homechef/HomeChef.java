c2939ce9dec968268930e34b66c6e89a6487b305
Gabriel Aguiar Cargnin <gabrielcargnin@hotmail.com>
2018-05-01 21:31:55 -0300

    alterando pom

--- HomeChef/src/main/java/src/main/java/br/ufsc/INE5608/homechef/HomeChef.java
+++ HomeChef/src/main/java/src/main/java/br/ufsc/INE5608/homechef/HomeChef.java
@@ -5,13 +5,7 @@
  */
 package src.main.java.br.ufsc.INE5608.homechef;
 
-import src.main.java.br.ufsc.INE5608.homechef.model.Ingrediente;
-import src.main.java.br.ufsc.INE5608.homechef.model.ListarIngredientesTableModel;
-import src.main.java.br.ufsc.INE5608.homechef.service.IngredienteService;
-import src.main.java.br.ufsc.INE5608.homechef.service.impl.IngredienteServiceImpl;
 import src.main.java.br.ufsc.INE5608.homechef.view.ListarIngredientes;
-
-import java.util.Collection;
 
 /**
  * @author Gabriel
@@ -22,9 +16,6 @@ public class HomeChef {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        IngredienteService ingredienteService = new IngredienteServiceImpl();
-        Collection<Ingrediente> ingredientes = ingredienteService.list();
-        ListarIngredientes listarIngredientes = new ListarIngredientes();
-        ListarIngredientesTableModel listarIngredientesTableModel = new ListarIngredientesTableModel(ingredientes);
+        new ListarIngredientes().setVisible(true);
     }
 }
